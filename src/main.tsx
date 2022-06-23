fc1d9dd802f5261cb55e914f987c49a7ce92867d
Rafael Lodi <rafaelcardeallodi@hotmail.com>
2022-06-22 21:27:03 -0300

    Roteamento e player da aula

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,3 @@
-import { ApolloProvider } from "@apollo/client";
-import { client } from "./lib/apollo";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
@@ -8,8 +6,6 @@ import "./styles/global.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <ApolloProvider client={client}>
-      <App />
-    </ApolloProvider>
+    <App />
   </React.StrictMode>
 );
