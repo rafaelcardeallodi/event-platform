9dd91e20fee88e63b9cc968a5cac9be9ed7f0bb7
Rafael Lodi <rafaelcardeallodi@hotmail.com>
2022-06-23 21:33:21 -0300

    Inscrição via GraphQL

--- src/lib/apollo.ts
+++ src/lib/apollo.ts
@@ -1,6 +1,9 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 export const client = new ApolloClient({
-  uri: "https://api-sa-east-1.graphcms.com/v2/cl4pci6bk1obz01yw6nsb3etf/master",
+  uri: import.meta.env.VITE_API_URL,
+  headers: {
+    Authorization: `Bearer ${import.meta.env.VITE_API_ACCESS_TOKEN}`,
+  },
   cache: new InMemoryCache(),
 });
