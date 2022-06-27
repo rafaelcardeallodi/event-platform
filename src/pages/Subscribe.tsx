085fce08fcaaab5ab4540aea93ca972e8e7cf3fb
Rafael Lodi <rafaelcardeallodi@hotmail.com>
2022-06-27 20:29:45 -0300

    Adicionando responsividade na página de subscribe

--- src/pages/Subscribe.tsx
+++ src/pages/Subscribe.tsx
@@ -31,23 +31,23 @@ export function Subscribe() {
 
   return (
     <div className="min-h-screen bg-blur bg-cover bg-no-repeat flex flex-col items-center">
-      <div className="w-full max-w-[1100px] flex items-center justify-between mt-20 mx-auto">
-        <div className="max-w-[640px]">
+      <div className="w-full max-w-[1100px] flex items-center justify-between mt-10 mx-auto flex-col md:flex-row md:mt-20">
+        <div className="max-w-[640px] flex flex-col items-center md:items-start px-6 md:px-0">
           <Logo />
 
-          <h1 className="mt-8 text-[2.5rem] leading-tight">
+          <h1 className="mt-8 text-[2.5rem] leading-tight text-center md:text-left">
             Construa uma{" "}
             <strong className="text-blue-500">aplicação completa</strong>, do
             zero, com <strong className="text-blue-500">React</strong>
           </h1>
-          <p className="mt-4 text-gray-200 leading-relaxed">
+          <p className="mt-4 text-gray-200 leading-relaxed text-center md:text-left">
             Em apenas uma semana você vai dominar na prática uma das tecnologias
             mais utilizadas e com alta demanda para acessar as melhores
             oportunidades do mercado.
           </p>
         </div>
 
-        <div className="p-8 bg-gray-700 border border-gray-500 rounded">
+        <div className="p-8 bg-gray-700 border border-gray-500 rounded w-full mt-8 md:w-auto md:mt-0">
           <strong className="text-2xl mb-6 block">
             Inscreva-se gratuitamente
           </strong>
